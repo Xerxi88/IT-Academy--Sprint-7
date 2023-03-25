589405d5ee6f2823753fbe874a6ed60073d04995
xerxi88 <serjicu88@gmail.com>
2023-03-25 18:14:48 +0100

    Nivell 1 Exercici 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [precioTotal, setPrecioTotal] = useState(0);
+  
+  let preu=0;
+
+
+  const modificar = (e) => {
+    if (e.target.id === "check1") {
+    preu = 500;
+  } else if (e.target.id === "check2") {
+    preu = 300;
+  } else if (e.target.id === "check3") {
+    preu = 200;
+  }
+    setPrecioTotal(e.target.checked ? precioTotal + preu : precioTotal - preu)
+    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>¿Que vols fer?</p>
+      <div>
+        <input type={"checkbox"} id="check1" onClick={modificar}></input>
+        <label for="check1">Una pàgina web (500 €)</label>
+      </div>
+      <div>
+        <input type={"checkbox"} id="check2" onClick={modificar}></input>
+        <label for="check2">Una campanya SEO (300 €)</label>
+      </div>
+      <div>
+        <input type={"checkbox"} id="check3" onClick={modificar}></input>
+        <label for="check3">Una campanya de publicitat (200 €)</label>
+      </div>
+
+      <p>Preu: {precioTotal} €</p>
+    </>
   );
-}
+};
 
 export default App;
